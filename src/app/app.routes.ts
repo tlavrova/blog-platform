ae6360a4acf08657e1242558f05f8b9f6d003401
tlavrova <tlavrova230@gmail.com>
2025-08-08 16:37:12 +0200

    add blog application layout and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Posts } from './components/posts/posts';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Posts },
+  { path: 'posts', component: Posts }
+];
