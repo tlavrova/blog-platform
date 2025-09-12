a335e36e0c1725f8a7c589e12a67b584c50b6386
tlavrova230 <tlavrova230@gmail.com>
2025-09-12 15:32:01 +0200

    restore environment.ts for deployment; update .gitignore to include environment file

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyAVpNF4xoyWMWc-wRZxcNGxDrsuzFQ290I",
+    authDomain: "blog-platform-fe6c4.firebaseapp.com",
+    projectId: "blog-platform-fe6c4",
+    storageBucket: "blog-platform-fe6c4.firebasestorage.app",
+    messagingSenderId: "153286335257",
+    appId: "1:153286335257:web:c8f43178a3eca3e9a4c388",
+    measurementId: "G-S4DT1H9BVX"
+  }
+};
+
