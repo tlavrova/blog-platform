da6d4b3d9142c64ec5e962082f97bfc155114f83
tlavrova230 <tlavrova230@gmail.com>
2025-09-12 15:16:43 +0200

    move Firebase config to environment file; update bootstrap to use environment config; add environment.ts to .gitignore

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyA7OpAUDfF-Oz40UZvZLjx8gkAB9xQQkWc",
+    authDomain: "blog-platform-6f6f6.firebaseapp.com",
+    projectId: "blog-platform-6f6f6",
+    storageBucket: "blog-platform-6f6f6.appspot.com",
+    messagingSenderId: "924217354444",
+    appId: "1:924217354444:web:8646b28866230ee9093a69",
+    measurementId: "G-1S2VL423SV"
+  }
+};
+
--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,13 @@ import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
-
-const firebaseConfig = {
-  apiKey: "", //publicity problem on github
-  authDomain: "blog-platform-6f6f6.firebaseapp.com",
-  projectId: "blog-platform-6f6f6",
-  storageBucket: "blog-platform-6f6f6.appspot.com",
-  messagingSenderId: "924217354444",
-  appId: "1:924217354444:web:8646b28866230ee9093a69",
-  measurementId: "G-1S2VL423SV"
-};
+import { environment } from './environments/environment';
 
 bootstrapApplication(App, {
   ...appConfig,
   providers: [
     ...(appConfig.providers || []),
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAuth(() => getAuth())
   ]
 })
