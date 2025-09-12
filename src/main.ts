00ed2351f7f9e4f340492817f399d530a0b1a555
tlavrova230 <tlavrova230@gmail.com>
2025-09-12 14:28:21 +0200

    add Firebase and AngularFire dependencies; initialize Firebase app and authentication in bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,25 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+const firebaseConfig = {
+  apiKey: "AIzaSyA7OpAUDfF-Oz40UZvZLjx8gkAB9xQQkWc",
+  authDomain: "blog-platform-6f6f6.firebaseapp.com",
+  projectId: "blog-platform-6f6f6",
+  storageBucket: "blog-platform-6f6f6.appspot.com",
+  messagingSenderId: "924217354444",
+  appId: "1:924217354444:web:8646b28866230ee9093a69",
+  measurementId: "G-1S2VL423SV"
+};
+
+bootstrapApplication(App, {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers || []),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth())
+  ]
+})
+.catch((err) => console.error(err));
