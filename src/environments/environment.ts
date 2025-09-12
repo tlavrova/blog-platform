6f5e3c5da496125efd84388d7a790c8969742d0f
tlavrova230 <tlavrova230@gmail.com>
2025-09-12 15:38:20 +0200

    update Firebase configuration in environment.ts with new project credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebaseConfig: {
-    apiKey: "AIzaSyAVpNF4xoyWMWc-wRZxcNGxDrsuzFQ290I",
-    authDomain: "blog-platform-fe6c4.firebaseapp.com",
-    projectId: "blog-platform-fe6c4",
-    storageBucket: "blog-platform-fe6c4.firebasestorage.app",
-    messagingSenderId: "153286335257",
-    appId: "1:153286335257:web:c8f43178a3eca3e9a4c388",
-    measurementId: "G-S4DT1H9BVX"
+    apiKey: "AIzaSyA-oHMEmbq2hGsmqDkJsAEbPlD0V10TRPc",
+    authDomain: "blog-platform-6f6f6.firebaseapp.com",
+    projectId: "blog-platform-6f6f6",
+    storageBucket: "blog-platform-6f6f6.firebasestorage.app",
+    messagingSenderId: "924217354444",
+    appId: "1:924217354444:web:8646b28866230ee9093a69",
+    measurementId: "G-1S2VL423SV"
   }
 };
 
